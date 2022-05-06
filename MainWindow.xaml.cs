df0171ef94f0ee243e60ad238fe7e7aee5b3c007
princegabz <gabrielkojo77@gmail.com>
2022-05-06 19:55:56 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prjWPFkfcICE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProductData pd = new ProductData();
+        Products p = new Products();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnStreetwise1_Click(object sender, RoutedEventArgs e)
+        {
+            p.Streetwise1(); //This adds the Price of one Streetwise 1 to the Total
+            MessageBox.Show(p.CartItems()+"Your Cart total is: R"+p.CartTotal(), "Cart Total", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+       
+        private void btnStreetwise2_Click(object sender, RoutedEventArgs e)
+        {
+            p.Streetwise2(); //This adds the Price of one Streetwise 2 to the Total
+            MessageBox.Show(p.CartItems() + "Your Cart total is: R" + p.CartTotal(), "Cart Total", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnStreetwise3_Click(object sender, RoutedEventArgs e)
+        {
+            p.Streetwise3(); //This adds the Price of one Streetwise 3 to the Total
+            MessageBox.Show(p.CartItems() + "Your Cart total is: R" + p.CartTotal(), "Cart Total", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnStreetwise4_Click(object sender, RoutedEventArgs e)
+        {
+            p.Streetwise4(); //This adds the Price of one Streetwise 3 to the Total
+            MessageBox.Show(p.CartItems() + "Your Cart total is: R" + p.CartTotal(), "Cart Total", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ ProductData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjWPFkfcICE
+{
+    class ProductData
+    {
+        public ProductData()
+        {
+
+        }
+
+        public static List<Products> lsProducts = new List<Products>();
+
+
+        
+    }
+
+    public class Products
+    {
+        public String[] ProductName= {"Streetwise 1", "Streetwise 2", "Streetwise 3" , "Streetwise 4" };
+        public int[] ProductPrice= {20,40,60,80};
+        public static int Total = 0; //Final Cart balance
+        public String Cart = "";
+
+        public void Streetwise1()
+        {
+            Total += ProductPrice[0];
+            Cart += ProductName[0] + "\n";
+        }
+        public void Streetwise2()
+        {
+            Total += ProductPrice[1];
+            Cart +=  ProductName[1] + "\n";
+        }
+        public void Streetwise3()
+        {
+            Total += ProductPrice[2];
+            Cart += ProductName[2] + "\n";
+        }
+        public void Streetwise4()
+        {
+            Total += ProductPrice[3];
+            Cart += ProductName[3] + "\n";
+        }
+
+        public int CartTotal()
+        {
+            return Total;
+        }
+        public String CartItems() //returns the items in the cart
+        {
+            return "You have ordered: \n" + Cart;
+        }
+
+    }
+   
+
+
+}
